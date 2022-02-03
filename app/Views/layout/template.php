9f5894cdecd1a2fbacfde6c2b6dccec30c896856
mohammn <mvapdg@gmail.com>
2022-02-03 10:33:47 +0700

    update template

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -109,10 +109,9 @@
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
-                    <form class="form-inline">
-                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                            <i class="fa fa-bars"></i>
-                        </button>
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
                     </form>
 
 
